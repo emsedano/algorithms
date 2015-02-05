630718a9bfad51ce2d921db2e294c865ddec6148
Elias Martinez <emsedano@live.com.mx>
2015-02-05 15:17:09 -0600

    Create QuickSort

---
+++ src/com/algorithms/sort/QuickSort.java
@@ -0,0 +1,21 @@
+package com.algorithms.sort;
+
+import com.algorithms.Problem;
+import com.algorithms.sort.ASortMachine;
+
+public class QuickSort extends ASortMachine {
+
+	public QuickSort(Problem problem) {
+		super(problem);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public Comparable[] sort(Comparable[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
