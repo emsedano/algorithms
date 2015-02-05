a6f79628af9adc2360cadb04892ed27f04b64f8c
Elias Martinez <emsedano@live.com.mx>
2015-02-05 15:18:06 -0600

    Create NoSearchMachineException

---
+++ src/com/algorithms/exceptions/NoSearchMachineException.java
@@ -0,0 +1,13 @@
+package com.algorithms.exceptions;
+
+public class NoSearchMachineException extends java.lang.Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1022277433116544014L;
+	
+	public NoSearchMachineException(){
+		super("No Search Machine found");
+	}
+}
