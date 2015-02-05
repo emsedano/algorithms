1d27ac0294b85a3510e70e94f56fb8e6cc39ca6f
Elias Martinez <emsedano@live.com.mx>
2015-02-05 15:18:27 -0600

    Create NoSortMachineException

---
+++ src/com/algorithms/exceptions/NoSortMachineException.java
@@ -0,0 +1,13 @@
+package com.algorithms.exceptions;
+
+public class NoSortMachineException extends java.lang.Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3515703945952263011L;
+
+	public NoSortMachineException(){
+		super("No Sort Machine found");
+	}
+}
